a84bc64174464cf0993f6e9e6885274e3e6f0b98
mingjia.ge <gemingjia0201@163.com>
2023-10-02 23:39:11 +0800

    update:

--- src/main/java/com/gearwenxin/service/ContService.java
+++ src/main/java/com/gearwenxin/service/ContService.java
@@ -1,9 +1,33 @@
 package com.gearwenxin.service;
 
+import com.gearwenxin.entity.response.ChatResponse;
+import com.gearwenxin.model.ContBot;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 /**
  * @author Ge Mingjia
  * @date 2023/9/27
  */
-public class ContService {
+public class ContService implements ContBot {
 
+    @Override
+    public Mono<ChatResponse> chatCont(String content, String msgUid) {
+        return null;
+    }
+
+    @Override
+    public Flux<ChatResponse> chatContOfStream(String content, String msgUid) {
+        return null;
+    }
+
+    @Override
+    public Mono<ChatResponse> chatCont(Object chatRequest, String msgUid) {
+        return null;
+    }
+
+    @Override
+    public Flux<ChatResponse> chatContOfStream(Object chatRequest, String msgUid) {
+        return null;
+    }
 }
