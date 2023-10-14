ad76c7950f874a9aa59be00413345de906e48098
Leon2young <leon2young@163.com>
2023-10-14 16:47:03 +0800

    add: 新增模型AquilaChat-7B

---
+++ src/main/java/com/gearwenxin/client/AquilaChat7BClient.java
@@ -0,0 +1,56 @@
+package com.gearwenxin.client;
+
+import com.gearwenxin.client.base.FullClient;
+import com.gearwenxin.common.Constant;
+import com.gearwenxin.entity.Message;
+
+import java.util.Map;
+import java.util.Deque;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Leon2young
+ * @date 2023/10/14
+ */
+public abstract class AquilaChat7BClient extends FullClient {
+
+    protected AquilaChat7BClient() {
+    }
+
+    private String accessToken = null;
+    private static final String TAG = "Aquila-Chat-7B-Client";
+    private static final String URL = Constant.AQUILA_CHAT_7B_URL;
+    private static Map<String, Deque<Message>> AQUILA_7B_MESSAGES_HISTORY_MAP = new ConcurrentHashMap<>();
+
+    protected abstract String getAccessToken();
+
+    @Override
+    public String getCustomAccessToken() {
+        return accessToken != null ? accessToken : getAccessToken();
+    }
+
+    @Override
+    public Map<String, Deque<Message>> getMessageHistoryMap() {
+        return AQUILA_7B_MESSAGES_HISTORY_MAP;
+    }
+
+    @Override
+    public void initMessageHistoryMap(Map<String, Deque<Message>> map) {
+        AQUILA_7B_MESSAGES_HISTORY_MAP = map;
+    }
+
+    @Override
+    public String getURL() {
+        return URL;
+    }
+
+    @Override
+    public void setCustomAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    @Override
+    public String getTag() {
+        return TAG;
+    }
+}
