03b73aceca55ea5d9c3cc532daec2a8320b4567f
mingjia.ge <gemingjia0201@163.com>
2023-10-15 17:50:20 +0800

    refactor: 命名规范性修改

--- src/main/java/com/gearwenxin/client/base/BaseClient.java
+++ src/main/java/com/gearwenxin/client/base/BaseClient.java
@@ -4,6 +4,7 @@ import com.gearwenxin.common.ErrorCode;
 import com.gearwenxin.entity.BaseProperty;
 import com.gearwenxin.entity.chatmodel.ChatBaseRequest;
 import com.gearwenxin.entity.chatmodel.ChatErnieRequest;
+import com.gearwenxin.entity.enums.ChatType;
 import com.gearwenxin.entity.response.ChatResponse;
 import com.gearwenxin.exception.BusinessException;
 import com.gearwenxin.model.BaseBot;
@@ -43,15 +44,15 @@ public abstract class BaseClient implements SingleBot, BaseBot {
                 .tag(getTag())
                 .accessToken(getCustomAccessToken())
                 .build();
-        clientMapMono.put("ErnieBotClientCS", this::chatSingleErnie);
-        clientMapMono.put("OtherClientCS", this::chatSingleDefault);
-        clientMapFlux.put("ErnieBotClientCSS", this::chatSingleOfStreamErnie);
-        clientMapFlux.put("OtherClientCSS", this::chatSingleOfStreamDefault);
+        clientMapMono.put("ErnieBotClient" + ChatType.once, this::chatSingleErnie);
+        clientMapMono.put("OtherClient" + ChatType.once, this::chatSingleDefault);
+        clientMapFlux.put("ErnieBotClient" + ChatType.contStream, this::chatSingleOfStreamErnie);
+        clientMapFlux.put("OtherClient" + ChatType.contStream, this::chatSingleOfStreamDefault);
     }
 
     @Override
     public Mono<ChatResponse> chatSingle(String content) {
-        Function<String, Mono<ChatResponse>> fluxFunction = clientMapMono.get(getTag() + "CS");
+        Function<String, Mono<ChatResponse>> fluxFunction = clientMapMono.get(getTag() + ChatType.once);
         if (fluxFunction != null) {
             return fluxFunction.apply(content);
         }
@@ -61,7 +62,7 @@ public abstract class BaseClient implements SingleBot, BaseBot {
     @Override
     public Flux<ChatResponse> chatSingleOfStream(String content) {
 
-        Function<String, Flux<ChatResponse>> fluxFunction = clientMapFlux.get(getTag() + "CSS");
+        Function<String, Flux<ChatResponse>> fluxFunction = clientMapFlux.get(getTag() + ChatType.contStream);
         if (fluxFunction != null) {
             return fluxFunction.apply(content);
         }
@@ -70,7 +71,7 @@ public abstract class BaseClient implements SingleBot, BaseBot {
 
     @Override
     public <T extends ChatBaseRequest> Mono<ChatResponse> chatSingle(T chatBaseRequest) {
-        Function<String, Mono<ChatResponse>> fluxFunction = clientMapMono.get(getTag() + "CS");
+        Function<String, Mono<ChatResponse>> fluxFunction = clientMapMono.get(getTag() + ChatType.once);
         if (fluxFunction != null) {
 //            return fluxFunction.apply(chatBaseRequest);
         }
@@ -79,7 +80,7 @@ public abstract class BaseClient implements SingleBot, BaseBot {
 
     @Override
     public <T extends ChatBaseRequest> Flux<ChatResponse> chatSingleOfStream(T chatBaseRequest) {
-        Function<String, Flux<ChatResponse>> fluxFunction = clientMapFlux.get(getTag() + "CSS");
+        Function<String, Flux<ChatResponse>> fluxFunction = clientMapFlux.get(getTag() + ChatType.contStream);
         if (fluxFunction != null) {
 //            return fluxFunction.apply(chatBaseRequest);
         }
