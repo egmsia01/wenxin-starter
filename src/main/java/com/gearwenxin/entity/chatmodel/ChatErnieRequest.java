889aca8093cd6c6f4bdeb07835ecac55c6b74f28
GMerge <gemingjia0201@163.com>
2023-10-26 14:27:42 +0800

    refactor:去除dev代码

--- src/main/java/com/gearwenxin/entity/chatmodel/ChatErnieRequest.java
+++ src/main/java/com/gearwenxin/entity/chatmodel/ChatErnieRequest.java
@@ -13,6 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.List;
 
 import static com.gearwenxin.common.Constant.MAX_CONTENT_LENGTH;
+import static com.gearwenxin.common.Constant.MAX_SYSTEM_LENGTH;
 
 /**
  * @author Ge Mingjia
@@ -54,36 +55,40 @@ public class ChatErnieRequest extends ChatBaseRequest {
      */
     private String system;
 
-    public void validSelf() {
-
-        // 检查content不为空
-        if (StringUtils.isBlank(getContent())) {
-            throw new WenXinException(ErrorCode.PARAMS_ERROR, "content cannot be empty");
-        }
-        // 检查单个content长度
-        if (getContent().length() > MAX_CONTENT_LENGTH) {
-            throw new WenXinException(ErrorCode.PARAMS_ERROR, "content's length cannot be more than 2000");
-        }
-        // 检查temperature和topP不都有值
-        if (getTemperature() != null && this.getTopP() != null) {
-            log.warn("Temperature and topP cannot both have value");
-        }
-        // 检查temperature范围
-        if (getTemperature() != null && (getTemperature() <= 0 || getTemperature() > 1.0)) {
-            throw new WenXinException(ErrorCode.PARAMS_ERROR, "temperature should be in (0, 1]");
-        }
-        // 检查topP范围
-        if (getTopP() != null && (getTopP() < 0 || getTopP() > 1.0)) {
-            throw new WenXinException(ErrorCode.PARAMS_ERROR, "topP should be in [0, 1]");
-        }
-        // 检查penaltyScore范围
-        if (getTemperature() != null && (getPenaltyScore() < 1.0 || getPenaltyScore() > 2.0)) {
-            throw new WenXinException(ErrorCode.PARAMS_ERROR, "penaltyScore should be in [1, 2]");
-        }
-        // 检查system与function call
-        if (StringUtils.isBlank(getSystem()) && getFunctions() != null) {
-            throw new WenXinException(ErrorCode.PARAMS_ERROR, "if 'function' not null, the 'system' must be null");
-        }
-
-    }
+//    public void validSelf() {
+//
+//        // 检查content不为空
+//        if (StringUtils.isBlank(getContent())) {
+//            throw new WenXinException(ErrorCode.PARAMS_ERROR, "content cannot be empty");
+//        }
+//        // 检查单个content长度
+//        if (getContent().length() > MAX_CONTENT_LENGTH) {
+//            throw new WenXinException(ErrorCode.PARAMS_ERROR, "content's length cannot be more than 2000");
+//        }
+//        // 检查temperature和topP不都有值
+//        if (getTemperature() != null && this.getTopP() != null) {
+//            log.warn("Temperature and topP cannot both have value");
+//        }
+//        // 检查temperature范围
+//        if (getTemperature() != null && (getTemperature() <= 0 || getTemperature() > 1.0)) {
+//            throw new WenXinException(ErrorCode.PARAMS_ERROR, "temperature should be in (0, 1]");
+//        }
+//        // 检查topP范围
+//        if (getTopP() != null && (getTopP() < 0 || getTopP() > 1.0)) {
+//            throw new WenXinException(ErrorCode.PARAMS_ERROR, "topP should be in [0, 1]");
+//        }
+//        // 检查penaltyScore范围
+//        if (getTemperature() != null && (getPenaltyScore() < 1.0 || getPenaltyScore() > 2.0)) {
+//            throw new WenXinException(ErrorCode.PARAMS_ERROR, "penaltyScore should be in [1, 2]");
+//        }
+//        // 检查system与function call
+//        if (StringUtils.isBlank(getSystem()) && getFunctions() != null) {
+//            throw new WenXinException(ErrorCode.PARAMS_ERROR, "if 'function' not null, the 'system' must be null");
+//        }
+//        // 检查system长度
+//        if (getSystem().length() > MAX_SYSTEM_LENGTH) {
+//            throw new WenXinException(ErrorCode.PARAMS_ERROR, "system's length cannot be more than 1024");
+//        }
+//
+//    }
 }
