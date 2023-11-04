8a9527abbebebca0d1420c6e4139cbfd0d940a89
GMerge <gemingjia0201@163.com>
2023-11-04 21:26:43 +0800

    fix: 修复url配置异常

--- src/main/java/com/gearwenxin/config/WenXinProperties.java
+++ src/main/java/com/gearwenxin/config/WenXinProperties.java
@@ -1,8 +1,5 @@
 package com.gearwenxin.config;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +7,7 @@ import org.springframework.stereotype.Component;
  * @author Ge Mingjia
  * @date 2023/11/1
  */
-@Data
+//@Data
 @Component
 @ConfigurationProperties("gear.wenxin")
 public class WenXinProperties {
@@ -50,6 +47,18 @@ public class WenXinProperties {
     private String star_coder_url;
     private String custom_model_url;
     private String sql_coder_7b_url;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
 
     public String getCommon_Url() {
         return common_url;
@@ -173,6 +182,142 @@ public class WenXinProperties {
 
     public String getSQLCoder_7B() {
         return sql_coder_7b_url;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public void setCommon_Url(String common_url) {
+        this.common_url = common_url;
+    }
+
+    public void setVilg_URL(String vilg_url) {
+        this.vilg_url = vilg_url;
+    }
+
+    public void setChatGLM2_6B_URL(String chat_glm2_6B_url) {
+        this.chat_glm2_6B_url = chat_glm2_6B_url;
+    }
+
+    public void setChatGLM2_6B_INT4_URL(String chat_glm2_6b_int4_url) {
+        this.chat_glm2_6b_int4_url = chat_glm2_6b_int4_url;
+    }
+
+    public void setVisual_GLM_6B_URL(String visual_glm_6b_url) {
+        this.visual_glm_6b_url = visual_glm_6b_url;
+    }
+
+    public void setLinly_Chinese_LLaMA2_7B_URL(String linly_chinese_llama2_7b_url) {
+        this.linly_chinese_llama2_7b_url = linly_chinese_llama2_7b_url;
+    }
+
+    public void setLinly_Chinese_LLaMA2_13B_URL(String linly_chinese_llama2_13b_url) {
+        this.linly_chinese_llama2_13b_url = linly_chinese_llama2_13b_url;
+    }
+
+    public void setFalcon_7B_URL(String falcon_7b_url) {
+        this.falcon_7b_url = falcon_7b_url;
+    }
+
+    public void setFalcon_40B_URL(String falcon_40b_url) {
+        this.falcon_40b_url = falcon_40b_url;
+    }
+
+    public void setRWKV4_World_URL(String rwky_4_world_url) {
+        this.rwky_4_world_url = rwky_4_world_url;
+    }
+
+    public void setRWKV5_World_URL(String rwky_5_world_url) {
+        this.rwky_5_world_url = rwky_5_world_url;
+    }
+
+    public void setRWKV4_pile_URL(String rwky_4_pile_url) {
+        this.rwky_4_pile_url = rwky_4_pile_url;
+    }
+
+    public void setRWKV_Raven_14B_URL(String rwky_raven_14b_url) {
+        this.rwky_raven_14b_url = rwky_raven_14b_url;
+    }
+
+    public void setOpen_LLaMA_7B_URL(String open_llama_7b_url) {
+        this.open_llama_7b_url = open_llama_7b_url;
+    }
+
+    public void setMPT_7B_Instruct_URL(String mpt_7b_instruct_url) {
+        this.mpt_7b_instruct_url = mpt_7b_instruct_url;
+    }
+
+    public void setMPT_30B_Instruct_URL(String mpt_30b_instruct_url) {
+        this.mpt_30b_instruct_url = mpt_30b_instruct_url;
+    }
+
+    public void setDolly_12B_URL(String dolly_12b_url) {
+        this.dolly_12b_url = dolly_12b_url;
+    }
+
+    public void setStable_Diffusion_V1_5_URL(String stable_diffusion_v1_5_url) {
+        this.stable_diffusion_v1_5_url = stable_diffusion_v1_5_url;
+    }
+
+    public void setFlan_UL2_URL(String flan_ul2_url) {
+        this.flan_ul2_url = flan_ul2_url;
+    }
+
+    public void setCerebras_GPT_13B_URL(String cerebras_gpt_13b_url) {
+        this.cerebras_gpt_13b_url = cerebras_gpt_13b_url;
+    }
+
+    public void setCerebras_GPT_6_7B_URL(String cerebras_gpt_6_7b_url) {
+        this.cerebras_gpt_6_7b_url = cerebras_gpt_6_7b_url;
+    }
+
+    public void setPythia_12B_URL(String pythia_12b_url) {
+        this.pythia_12b_url = pythia_12b_url;
+    }
+
+    public void setPythia_6_9B_URL(String pythia_6_9b_url) {
+        this.pythia_6_9b_url = pythia_6_9b_url;
+    }
+
+    public void setGPT_J_6B_URL(String gpt_j_6b_url) {
+        this.gpt_j_6b_url = gpt_j_6b_url;
+    }
+
+    public void setGPT_NeoX_20B_URL(String gpt_neox_20b_url) {
+        this.gpt_neox_20b_url = gpt_neox_20b_url;
+    }
+
+    public void setOA_Pythia_12B_SFT4_URL(String oa_pythia_12b_sft_4_url) {
+        this.oa_pythia_12b_sft_4_url = oa_pythia_12b_sft_4_url;
+    }
+
+    public void setGPT4All_J_URL(String gpt4all_j_url) {
+        this.gpt4all_j_url = gpt4all_j_url;
+    }
+
+    public void setStableLM_Alpha_7B_URL(String stablelm_alpha_7b_url) {
+        this.stablelm_alpha_7b_url = stablelm_alpha_7b_url;
+    }
+
+    public void setStarCoder_URL(String star_coder_url) {
+        this.star_coder_url = star_coder_url;
+    }
+
+    public void setCustom_Model_URL(String custom_model_url) {
+        this.custom_model_url = custom_model_url;
+    }
+
+    public void setSQLCoder_7B(String sql_coder_7b_url) {
+        this.sql_coder_7b_url = sql_coder_7b_url;
     }
 
 }
