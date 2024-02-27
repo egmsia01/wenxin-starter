e834bf0689f5c112d4a3bd3621eef16e4c128390
GMerge <gemingjia0201@163.com>
2024-02-27 22:03:31 +0800

    refactor: 尝试重构SDK

---
+++ src/main/java/com/gearwenxin/client/ClientFactory.java
@@ -0,0 +1,13 @@
+package com.gearwenxin.client;
+
+/**
+ * @author GMerge
+ * {@code @date} 2024/2/27
+ */
+public class ClientFactory {
+
+    public void getClient(String modelName){
+
+    }
+
+}
